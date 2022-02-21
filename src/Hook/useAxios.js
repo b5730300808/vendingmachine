c9b9e3f9ed66ccb1dcc4066f36de82791e70df43
jobjab <jab.for.j@gmail.com>
2022-02-21 07:52:18 +0700

    update localhost

--- src/Hook/useAxios.js
+++ src/Hook/useAxios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url = "https://vendingmachine-api-jobjab.herokuapp.com"
+const url = "http://localhost:9000"
 const instance = axios.create({
     baseURL: url,
     headers: {
